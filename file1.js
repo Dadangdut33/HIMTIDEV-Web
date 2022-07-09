0787fecaa137e81401910a9ea2669cf65760e98f
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-07-09 11:13:46 +0700

    big bang 💥

---
+++ file1.js
@@ -0,0 +1,7 @@
+const a = "hello";
+const b = () => {
+	console.log("hello world");
+};
+const c = {};
+
+module.exports = { a, b, c };
---
+++ file2.js
@@ -0,0 +1,6 @@
+const { a, b, c } = require("./file1");
+
+console.log(a);
+b();
+console.log(c);
+console.log("file2");
